1b120c2d40fc39fd7a3ec9f6813ab6dc209d5d68
w_xuchaoyang <w_xuchaoyang@kingsoft.com>
2026-03-17 20:51:04 +0800

    feat: 字符串流式输出

--- src/ch09/ai-research-agent/config/model.js
+++ src/ch09/ai-research-agent/config/model.js
@@ -5,6 +5,7 @@ const model = new ChatDeepSeek({
   apiKey: process.env.DEEPSEEK_API_KEY,
   modelName: "deepseek-chat",
   temperature: 0.7,
+  streaming: true
 })
 
 export default model
--- src/ch09/ai-research-agent/index.js
+++ src/ch09/ai-research-agent/index.js
@@ -1,15 +1,24 @@
 import { app } from './graph/agentGraph.js'
 
 async function main(){
-  const response = await app.invoke({
+  const stream = await app.streamEvents({
     messages:[
       {
         role:'human',
         content:'未来 AI 对前端开发有什么影响？'
       }
-    ]
-  })
-  console.log(response)
+    ],
+  }, {
+      version: "v2",
+    })
+  for await (const event of stream) {
+    if (event.event === "on_chat_model_stream") {
+      const token = event.data?.chunk?.content
+      if (token) {
+        process.stdout.write(token)
+      }
+    }
+  }
 }
 
 main()
